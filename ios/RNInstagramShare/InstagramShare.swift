d6884d93b9ea423055553bdeaf2928b7099db8c2
IndianAg0711 <joseph.villafranca@gmail.com>
2017-05-28 10:36:52 -0500

    update to use shared application root view

--- ios/RNInstagramShare/InstagramShare.swift
+++ ios/RNInstagramShare/InstagramShare.swift
@@ -14,13 +14,13 @@ class InstagramShare: UIDocumentInteractionController, UIDocumentInteractionCont
   
   @objc func share(filename: String) -> Void {
     
-    NSLog("%@", filename)
-    
-    self.url = URL(fileURLWithPath: filename)
+    if let viewController = UIApplication.shared.keyWindow?.rootViewController {
+      url = URL(fileURLWithPath: filename)
       
-    self.delegate = self as! UIDocumentInteractionControllerDelegate
-    self.uti = "com.instagram.exclusivegram"
-    self.presentOpenInMenu(from: CGRect(x: 0, y: 0, width: 0, height: 0), in: self.view, animated: false)
+      delegate = self
+      uti = "com.instagram.exclusivegram"
+      presentOpenInMenu(from: CGRect(x: 0, y: 0, width: 0, height: 0), in: (viewController.view), animated: true)
+    }
   }
   
 }
